43dafd302d54fbc0f7190f3caef01cc6b90bd6ea
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2022-12-23 14:54:32 +0200

    Move from `pages` to `app`

---
+++ app/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+
+export default function Short() {
+  return (
+    <>
+      <h1>Hi</h1>
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Rubik } from "@next/font/google";
+
+const inter = Rubik({ subsets: ["hebrew"] });
+
+export default function Home() {
+  return (
+    <main>
+      <h1>הנטאי כחול לבן</h1>
+    </main>
+  );
+}
