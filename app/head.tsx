cfd3276967acd5556b6d714fbbc654bee52cb617
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2022-12-29 19:44:13 +0200

    Adds OG image

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <title>הנטאי.ישראל</title>
+      <meta property="og:image" content="/public/og.jpg" />
       <meta name="description" content="הנטאי. כחול לבן." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
