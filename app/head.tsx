6aaebbcc633d0378a42fe82d115ff2c264816bc3
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2022-12-29 20:36:53 +0200

    Fix og tags again

--- app/head.tsx
+++ app/head.tsx
@@ -1,15 +1,26 @@
+const SITE_URL = "https://hentai-israel.vercel.app";
+
 export default function Head() {
   return (
     <>
       <title>הנטאי.ישראל</title>
+
       <meta property="og:title" content="הנטאי.ישראל" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://hentai-israel.vercel.app" />
+      <meta property="og:url" content={SITE_URL} />
       <meta property="og:locale" content="he_IL" />
-      <meta property="og:image" content="https://hentai-israel.vercel.app/og.jpg" />
+      <meta property="og:image" content={`${SITE_URL}/og.jpg`} />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={SITE_URL} />
+      <meta property="twitter:title" content="הנטאי.ישראל" />
+      <meta property="twitter:description" content="הנטאי. כחול לבן." />
+      <meta property="twitter:image" content={`${SITE_URL}/og.jpg`} />
+
       <meta name="description" content="הנטאי. כחול לבן." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
