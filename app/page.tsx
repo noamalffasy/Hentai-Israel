6fb970b92f125fb65216f663e8b062cff071e4b7
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2023-01-04 21:38:02 +0200

    Updates the first title to be a <h1>

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <div className="lg:max-w-8xl mx-auto flex w-full max-w-2xl flex-1 flex-col justify-center px-4 py-12 md:py-24 lg:px-8 xl:px-12">
         <div className="space-y-10">
           <div className="text-center">
-            <p className="inline bg-gradient-to-r from-blue-700 to-blue-400 bg-clip-text text-5xl font-semibold tracking-tight text-transparent md:text-7xl">
+            <h1 className="inline bg-gradient-to-r from-blue-700 to-blue-400 bg-clip-text text-5xl font-semibold tracking-tight text-transparent md:text-7xl">
               הנטאי.ישראל
-            </p>
+            </h1>
             <p className="mt-3 text-xl tracking-tight text-slate-500 md:text-4xl">
               הנטאי כחול לבן
             </p>
