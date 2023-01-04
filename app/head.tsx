c60c5cd6822eeae69f519e11a29943917aff55ed
Noam Alffasy <7583207+noamalffasy@users.noreply.github.com>
2023-01-04 21:40:03 +0200

    Update description

--- app/head.tsx
+++ app/head.tsx
@@ -4,20 +4,20 @@ export default function Head() {
   return (
     <>
       <title>הנטאי.ישראל</title>
+      <meta name="description" content="הנטאי. כחול לבן. מקצר הקישורים הישראלי ביותר." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <meta property="og:title" content="הנטאי.ישראל" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={SITE_URL} />
       <meta property="og:locale" content="he_IL" />
       <meta property="og:image" content={`${SITE_URL}/og.jpg`} />
+
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={SITE_URL} />
       <meta property="twitter:title" content="הנטאי.ישראל" />
       <meta property="twitter:description" content="הנטאי. כחול לבן." />
       <meta property="twitter:image" content={`${SITE_URL}/og.jpg`} />
-
-      <meta name="description" content="הנטאי. כחול לבן." />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
